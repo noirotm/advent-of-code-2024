e8182313f7b82bc395f30c77ff683e99a0a4ca9b
Marc Noirot <marc.noirot@gmail.com>
2024-12-03 14:10:00 +0100

    Add solution for day 3

---
+++ src/solutions/day03.rs
@@ -0,0 +1,59 @@
+use crate::solver::Solver;
+use regex::RegexBuilder;
+use std::io::Read;
+
+pub struct Problem;
+
+impl Solver for Problem {
+    type Input = String;
+    type Output1 = u64;
+    type Output2 = u64;
+
+    fn parse_input<R: Read>(&self, mut r: R) -> anyhow::Result<Self::Input> {
+        let mut s = String::new();
+        r.read_to_string(&mut s)?;
+        Ok(s)
+    }
+
+    fn solve_first(&self, input: &Self::Input) -> Self::Output1 {
+        mul_total(input)
+    }
+
+    fn solve_second(&self, input: &Self::Input) -> Self::Output2 {
+        mul_total(&filter_string(input))
+    }
+}
+
+fn mul_total(s: &str) -> u64 {
+    let re = RegexBuilder::new("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        .multi_line(true)
+        .build()
+        .unwrap();
+
+    re.captures_iter(s)
+        .map(|c| (c[1].parse::<u64>().unwrap(), c[2].parse::<u64>().unwrap()))
+        .map(|(x, y)| x * y)
+        .sum()
+}
+
+fn filter_string(s: &str) -> String {
+    let mut enabled = true;
+    let mut cursor = s;
+    let mut result = String::new();
+
+    while !cursor.is_empty() {
+        if enabled {
+            if cursor.get(0..7).unwrap_or_default() == "don't()" {
+                enabled = false;
+            } else {
+                result.push_str(&cursor[0..1]);
+            }
+        } else if cursor.get(0..4).unwrap_or_default() == "do()" {
+            enabled = true;
+        }
+
+        cursor = &cursor[1..];
+    }
+
+    result
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,12 +3,14 @@ use crate::solver::Solver;
 
 mod day01;
 mod day02;
+mod day03;
 
 
 pub fn exec_day(day: u32) {
     match day {
         1 => day01::Problem {}.solve(day),
         2 => day02::Problem {}.solve(day),
+        3 => day03::Problem {}.solve(day),
 
         d => println!("Day {d} hasn't been solved yet :("),
     }
@@ -19,4 +21,6 @@ pub fn exec_all_days() {
     day01::Problem {}.solve(1);
     println!("Day 2:");
     day02::Problem {}.solve(2);
+    println!("Day 3:");
+    day03::Problem {}.solve(3);
 }
