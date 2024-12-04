85096f3993fe309e8cdacba28e2c99a59bcaf478
Marc Noirot <marc.noirot@gmail.com>
2024-12-04 18:34:20 +0100

    Improve reporting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use crate::solutions::{exec_all_days, exec_day};
+use humantime::format_duration;
 use std::env;
+use std::time::Duration;
 
 mod grid;
 mod parsing;
@@ -8,8 +10,29 @@ mod solver;
 
 fn main() {
     if let Some(day) = env::args().nth(1) {
-        exec_day(day.parse().unwrap_or(1));
+        if let Some(o) = exec_day(day.parse().unwrap_or(1)) {
+            println!("{o}");
+        } else {
+            eprintln!("Day {day} not found");
+        }
     } else {
-        exec_all_days();
+        let outputs = exec_all_days();
+        for (i, o) in outputs.iter().enumerate() {
+            println!("=== Day {} ===\n{o}", i + 1);
+        }
+
+        println!("=== Global stats ===");
+        let total_parsed = outputs
+            .iter()
+            .map(|o| o.timings.parse_duration)
+            .sum::<Duration>();
+        let total_solved = outputs
+            .iter()
+            .map(|o| o.timings.part1_duration + o.timings.part2_duration)
+            .sum::<Duration>();
+
+        println!("Parsing: {}", format_duration(total_parsed));
+        println!("Solving: {}", format_duration(total_solved));
+        println!("Total:   {}", format_duration(total_parsed + total_solved));
     }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,12 +1,46 @@
 use humantime::format_duration;
-use std::fmt::Display;
+use std::fmt::{Display, Formatter};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 fn input_file(day: u32) -> String {
     format!("input/{:02}.txt", day)
+}
+
+pub struct Timings {
+    pub parse_duration: Duration,
+    pub part1_duration: Duration,
+    pub part2_duration: Duration,
+}
+
+pub struct ProblemOutput {
+    pub part1: String,
+    pub part2: String,
+    pub timings: Timings,
+}
+
+impl Display for ProblemOutput {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "Parsing: ({})",
+            format_duration(self.timings.parse_duration)
+        )?;
+        writeln!(
+            f,
+            "Part 1: {:<20} ({})",
+            self.part1,
+            format_duration(self.timings.part1_duration)
+        )?;
+        write!(
+            f,
+            "Part 2: {:<20} ({})",
+            self.part2,
+            format_duration(self.timings.part2_duration)
+        )
+    }
 }
 
 pub trait Solver {
@@ -23,24 +57,31 @@ pub trait Solver {
         self.parse_input(f)
     }
 
-    fn solve(&self, day: u32) {
+    fn solve(&self, day: u32) -> ProblemOutput {
         let input_file = input_file(day);
 
         let start = Instant::now();
         let input = self
             .load_input(input_file)
             .expect("unable to open input file");
-        let time = start.elapsed();
-        println!("Parsing: ({})", format_duration(time));
+        let parse_duration = start.elapsed();
 
         let start = Instant::now();
         let s1 = self.solve_first(&input);
-        let time = start.elapsed();
-        println!("Solution 1: {:<20} ({})", s1, format_duration(time));
+        let part1_duration = start.elapsed();
 
         let start = Instant::now();
         let s2 = self.solve_second(&input);
-        let time = start.elapsed();
-        println!("Solution 2: {:<20} ({})", s2, format_duration(time));
+        let part2_duration = start.elapsed();
+
+        ProblemOutput {
+            part1: s1.to_string(),
+            part2: s2.to_string(),
+            timings: Timings {
+                parse_duration,
+                part1_duration,
+                part2_duration,
+            },
+        }
     }
 }
